96ca34347eac4cea5e66fbe997078af0374a486f
Hasmita Kurre <61547475+HasmitaKurre@users.noreply.github.com>
2024-07-22 02:27:34 +0530

    Add files via upload

---
+++ hack.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "libGameLogic.h"
+using namespace std;
+
+#include <dlfcn.h>
+#include <set>
+#include <map>
+#include <functional>
+#include <string>
+#include <vector>
+#include<iostream>
+#include "libGameLogic.h"
+
+void Player::SetJumpState(bool b){
+    printf("[*] SetJumpState(%d)\n",b);
+}
+
+int main()
+{
+	std::cout << "Header is Ready!";
+	return 0;
+}
